91770e3536dc47b2da03f2f5068e3d7956214634
KeyLime17 <nesterenka.makar@gmail.com>
2025-04-07 21:10:36 -0400

    issue with $symbol

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -9,9 +9,15 @@
         .down {
             color: red;
         }
-        table, tr, th {border: 1px solid black;}
-        td, th {padding: 10px;}
-        table {border-collapse: collapse;}
+        table, tr, th {
+            border: 1px solid black;
+        }
+        td, th {
+            padding: 10px;
+        }
+        table {
+            border-collapse: collapse;
+        }
     </style>
 </head>
 <body>
@@ -19,17 +25,23 @@
     <p>Here is your cryptocurrency newsletter with the latest crypto prices:</p>
 
     <table>
-        <tr><th>Symbol</th><td>{{ $symbol }}</td></tr>
-        <tr><th>Price (USD)</th><td>{{ $price_usd }}</td></tr>
-        <tr><th>Percent Change (24h)</th>
-            <td @class(['up' => $percent_change_24h > 0, 
-                'down' => $percent_change_24h < 0])>
-                {{ $percent_change_1h }}
+        <tr>
+            <th>Symbol</th>
+            <td>{{ $symbol }}</td>
+        </tr>
+        <tr>
+            <th>Price (USD)</th>
+            <td>{{ $price_usd }}</td>
+        </tr>
+        <tr>
+            <th>Percent Change (24h)</th>
+            <td @class(['up' => $percent_change_24h > 0, 'down' => $percent_change_24h < 0])>
+                {{ $percent_change_24h }}
             </td>
         </tr>
-        <tr><th>Percent Change (1h)</th>
-            <td @class(['up' => $percent_change_1h > 0, 
-                'down' => $percent_change_1h < 0])>
+        <tr>
+            <th>Percent Change (1h)</th>
+            <td @class(['up' => $percent_change_1h > 0, 'down' => $percent_change_1h < 0])>
                 {{ $percent_change_1h }}
             </td>
         </tr>
@@ -38,9 +50,8 @@
     <h2>All Tickers</h2>
     <ul>
         @foreach ($alltickers as $ticker)
-            <li>{{ $ticker->symbol }}</li>
+            <li>{{ $ticker['symbol'] }}</li>
         @endforeach
     </ul>
-
 </body>
 </html>
